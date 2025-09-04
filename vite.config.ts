d7a52365770f67599430b033b27697f57be544c1
lukasz-glen <lukasz.glen@poczta.fm>
2025-09-04 12:12:55 +0200

    fix(ci): contract tests are not vitest based - excluded

--- vite.config.ts
+++ vite.config.ts
@@ -91,6 +91,7 @@ export default defineConfig(({ mode }) => ({
       '**/.{idea,git,cache,output,temp}/**',
       '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
       '**/e2e/**',
+      'contracts/**',
     ],
     environment: 'jsdom',
   },
